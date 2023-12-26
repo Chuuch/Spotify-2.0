03a5c6e5f0f857790d6858c59de1750c96caada4
Chuuch <daniel.chuchulev96@gmail.com>
2023-12-26 15:39:09 +0200

    Added auth modal to library & check if user is not logged in

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,10 +1,22 @@
 'use client';
 import { TbPlaylist } from 'react-icons/tb';
 import { AiOutlinePlus } from 'react-icons/ai';
+import useAuthModal from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUser';
+import useUploadModal from '@/hooks/useUploadModal';
 
 const Library = () => {
+	const authModal = useAuthModal();
+	const uploadModal = useUploadModal();
+	const { user } = useUser();
 	const onClick = () => {
-		// Handle upload later
+		if (!user) {
+			return authModal.onOpen();
+		}
+
+		//TODO: Check for subscription
+
+		return uploadModal.onOpen();
 	};
 	return (
 		<div className="flex flex-col">
@@ -19,9 +31,7 @@ const Library = () => {
 					className="text-neutral-400 cursor-pointer hover:text-white transition"
 				/>
 			</div>
-            <div className='flex flex-col gap-y-2 mt-4 px-3'>
-                List of Songs!
-            </div>
+			<div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
 		</div>
 	);
 };
