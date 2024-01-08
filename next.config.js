41f098171ba5ebbda6d2fab86b2509073a735d66
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-07 22:36:50 -0800

    fixed images domains in next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'sbmfrmcyxnesjlaoydnz.supabase.co'
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'sbmfrmcyxnesjlaoydnz.supabase.co',
+            }
         ]
     }
 }
